6cf756ac23132c3aa6179b2d256870614c892afa
braintop <braintop7@gmail.com>
2025-09-03 21:09:37 +0300

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
 import redImg from "../assets/images/red.png";
 
 export default function Home() {
-
     return (
         <div>
             <h1>Welcome to the Home page</h1>
 
-            <img src={redImg} alt="red" />
+            <img src="../assets/images/red.png" alt="red" />
         </div>
     )
 }
