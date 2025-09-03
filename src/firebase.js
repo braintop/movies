cc58219afc84dfeec8b1294c5b2c2b8669b7e5e4
braintop <braintop7@gmail.com>
2025-09-03 20:06:44 +0300

    add

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDzhBqcn01o9phGvJyVoXVrDeff5j86fsM",
-  authDomain: "testmemo-2ad0c.firebaseapp.com",
-  projectId: "testmemo-2ad0c",
-  storageBucket: "testmemo-2ad0c.firebasestorage.app",
-  messagingSenderId: "409768253762",
-  appId: "1:409768253762:web:38647b4557abf8ce0bd0db"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
